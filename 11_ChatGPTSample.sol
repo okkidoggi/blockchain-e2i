58746d9035edb11de7699f0714a2ab4695d540e9
Teng Wei Yuen <tengweiyuen@hotmail.com>
2024-01-16 12:39:21 +0800

    Update 11_ChatGPTSample.sol

--- 11_ChatGPTSample.sol
+++ 11_ChatGPTSample.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ProductOwnership {
+    address public owner;
+    
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Invalid new owner address");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+
+    function getCurrentOwner() external view returns (address) {
+        return owner;
+    }
+}
 
